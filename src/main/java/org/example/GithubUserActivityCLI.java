8440d2af05bce8b209432d41950a1e2477fdbe86
Mohit kumar <mohitk02082000@gmail.com>
2025-07-29 17:00:09 +0530

    initial commit

---
+++ src/main/java/org/example/GithubUserActivityCLI.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class GithubUserActivityCLI {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the GitHub User Activity CLI!");
+    }
+}
